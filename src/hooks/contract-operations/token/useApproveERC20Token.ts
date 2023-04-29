dc42d5f71656f6790143c9626d73ebda051e346f
0xmegalodon <dung.thanh@autonomous.nyc>
2023-04-29 11:57:41 +0700

    add approve token

--- src/hooks/contract-operations/token/useApproveERC20Token.ts
+++ src/hooks/contract-operations/token/useApproveERC20Token.ts
@@ -1,10 +1,15 @@
-import {ContractOperationHook, DAppType} from '@/interfaces/contract-operation';
+import { ContractOperationHook, DAppType } from '@/interfaces/contract-operation';
 import ERC20ABIJson from '@/abis/erc20.json';
-import {useWeb3React} from '@web3-react/core';
-import {useCallback} from 'react';
-import {getContract} from '@/utils';
+import { useWeb3React } from '@web3-react/core';
+import { useCallback, useContext } from 'react';
+import { AssetsContext } from '@/contexts/assets-context';
+import * as TC_SDK from 'trustless-computer-sdk';
+import BigNumber from 'bignumber.js';
+import { formatBTCPrice } from '@/utils/format';
+import { getContract } from '@/utils';
+import { TRANSFER_TX_SIZE } from '@/configs';
 import Web3 from 'web3';
-import {TransactionEventType} from '@/enums/transaction';
+import { TransactionEventType } from '@/enums/transaction';
 
 export interface IApproveERC20TokenParams {
   address: string;
@@ -14,12 +19,30 @@ export interface IApproveERC20TokenParams {
 
 const useApproveERC20Token: ContractOperationHook<IApproveERC20TokenParams, boolean> = () => {
   const { account, provider } = useWeb3React();
+  const { btcBalance, feeRate } = useContext(AssetsContext);
 
   const call = useCallback(
     async (params: IApproveERC20TokenParams): Promise<boolean> => {
       const { address, amount, erc20TokenAddress } = params;
       if (account && provider && erc20TokenAddress) {
         const contract = getContract(erc20TokenAddress, ERC20ABIJson.abi, provider, account);
+        console.log({
+          tcTxSizeByte: TRANSFER_TX_SIZE,
+          feeRatePerByte: feeRate.fastestFee,
+          erc20TokenAddress,
+        });
+        const estimatedFee = TC_SDK.estimateInscribeFee({
+          tcTxSizeByte: TRANSFER_TX_SIZE,
+          feeRatePerByte: feeRate.fastestFee,
+        });
+        const balanceInBN = new BigNumber(btcBalance);
+        if (balanceInBN.isLessThan(estimatedFee.totalFee)) {
+          throw Error(
+            `Your balance is insufficient. Please top up at least ${formatBTCPrice(
+              estimatedFee.totalFee.toString(),
+            )} BTC to pay network fee.`,
+          );
+        }
 
         const transaction = await contract
           .connect(provider.getSigner())
@@ -30,7 +53,7 @@ const useApproveERC20Token: ContractOperationHook<IApproveERC20TokenParams, bool
 
       return false;
     },
-    [account, provider],
+    [account, provider, btcBalance, feeRate],
   );
 
   return {
