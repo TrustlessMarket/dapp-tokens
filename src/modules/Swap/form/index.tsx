49028c1834cf9f4c5a6d7315c4f0a4ca000dd725
0xmegalodon <dung.thanh@autonomous.nyc>
2023-05-01 17:08:50 +0700

    fix error

--- src/modules/Swap/form/index.tsx
+++ src/modules/Swap/form/index.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/no-children-prop */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { transactionType } from '@/components/Swap/alertInfoProcessing/types';
+import {transactionType} from '@/components/Swap/alertInfoProcessing/types';
 import FiledButton from '@/components/Swap/button/filedButton';
 import FilterButton from '@/components/Swap/filterToken';
 import FieldAmount from '@/components/Swap/form/fieldAmount';
@@ -9,55 +9,43 @@ import InputWrapper from '@/components/Swap/form/inputWrapper';
 import HorizontalItem from '@/components/Swap/horizontalItem';
 import SlippageSettingButton from '@/components/Swap/slippageSetting/button';
 import WrapperConnected from '@/components/WrapperConnected';
-import { UNIV2_ROUTER_ADDRESS } from '@/configs';
-import {
-  BRIDGE_SUPPORT_TOKEN,
-  TRUSTLESS_BRIDGE,
-  TRUSTLESS_FAUCET,
-} from '@/constants/common';
-import { NULL_ADDRESS } from '@/constants/url';
-import { AssetsContext } from '@/contexts/assets-context';
+import {UNIV2_ROUTER_ADDRESS} from '@/configs';
+import {BRIDGE_SUPPORT_TOKEN, TRUSTLESS_BRIDGE, TRUSTLESS_FAUCET,} from '@/constants/common';
+import {NULL_ADDRESS} from '@/constants/url';
+import {AssetsContext} from '@/contexts/assets-context';
 import useGetPair from '@/hooks/contract-operations/swap/useGetPair';
 import useGetReserves from '@/hooks/contract-operations/swap/useReserves';
 import useSwapERC20Token from '@/hooks/contract-operations/swap/useSwapERC20Token';
 import useApproveERC20Token from '@/hooks/contract-operations/token/useApproveERC20Token';
 import useBalanceERC20Token from '@/hooks/contract-operations/token/useBalanceERC20Token';
 import useIsApproveERC20Token from '@/hooks/contract-operations/token/useIsApproveERC20Token';
-import { IToken } from '@/interfaces/token';
-import { TransactionStatus } from '@/interfaces/walletTransaction';
-import { getSwapTokens } from '@/services/token-explorer';
-import { useAppDispatch, useAppSelector } from '@/state/hooks';
+import {IToken} from '@/interfaces/token';
+import {TransactionStatus} from '@/interfaces/walletTransaction';
+import {getSwapTokens} from '@/services/token-explorer';
+import {useAppDispatch, useAppSelector} from '@/state/hooks';
 import {
   requestReload,
   requestReloadRealtime,
   selectPnftExchange,
   updateCurrentTransaction,
 } from '@/state/pnftExchange';
-import { getIsAuthenticatedSelector, getUserSelector } from '@/state/user/selector';
-import { camelCaseKeys, compareString, formatCurrency } from '@/utils';
-import { isDevelop } from '@/utils/commons';
-import { composeValidators, required } from '@/utils/formValidate';
-import { formatEthPrice } from '@/utils/format';
+import {getIsAuthenticatedSelector, getUserSelector} from '@/state/user/selector';
+import {camelCaseKeys, compareString, formatCurrency} from '@/utils';
+import {isDevelop} from '@/utils/commons';
+import {composeValidators, required} from '@/utils/formValidate';
+import {formatEthPrice} from '@/utils/format';
 import px2rem from '@/utils/px2rem';
-import { showError } from '@/utils/toast';
-import { Box, Flex, Text, forwardRef } from '@chakra-ui/react';
+import {showError} from '@/utils/toast';
+import {Box, Flex, forwardRef, Text} from '@chakra-ui/react';
 import BigNumber from 'bignumber.js';
 import cx from 'classnames';
-import { isEmpty } from 'lodash';
 import debounce from 'lodash/debounce';
 import Link from 'next/link';
-import {
-  useCallback,
-  useContext,
-  useEffect,
-  useImperativeHandle,
-  useRef,
-  useState,
-} from 'react';
-import { Field, Form, useForm, useFormState } from 'react-final-form';
+import {useCallback, useContext, useEffect, useImperativeHandle, useRef, useState,} from 'react';
+import {Field, Form, useForm, useFormState} from 'react-final-form';
 import toast from 'react-hot-toast';
-import { RiArrowUpDownLine } from 'react-icons/ri';
-import { useDispatch, useSelector } from 'react-redux';
+import {RiArrowUpDownLine} from 'react-icons/ri';
+import {useDispatch, useSelector} from 'react-redux';
 import styles from './styles.module.scss';
 
 const LIMIT_PAGE = 50;
@@ -419,6 +407,7 @@ export const MakeFormSwap = forwardRef((props, ref) => {
     const numerator = amountInWithFee.multipliedBy(reserveOut);
     const denominator = reserveIn.multipliedBy(1000).plus(amountInWithFee);
     const amountOut = numerator.div(denominator);
+
     return amountOut;
   };
 
@@ -448,7 +437,7 @@ export const MakeFormSwap = forwardRef((props, ref) => {
     }
     const quoteAmount = getAmountOut(amountIn, reserveIn, reserveOut);
 
-    change('quoteAmount', quoteAmount.toString());
+    change('quoteAmount', quoteAmount.toFixed());
   };
 
   const onChangeValueQuoteAmount = (amount: any) => {
@@ -477,7 +466,7 @@ export const MakeFormSwap = forwardRef((props, ref) => {
     }
     const baseAmount = getAmountOut(amountIn, reserveIn, reserveOut);
 
-    change('baseAmount', baseAmount.toString());
+    change('baseAmount', baseAmount.toFixed());
   };
 
   const handleChangeMaxBaseAmount = () => {
