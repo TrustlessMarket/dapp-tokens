0b82e9b5557297e0461aa09e3e84908a768a9bf9
0xmegalodon <dung.thanh@autonomous.nyc>
2023-05-17 17:04:33 +0700

    update search

--- src/modules/Tokens/index.tsx
+++ src/modules/Tokens/index.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-empty-function */
 import Button from '@/components/Button';
 import {IToken} from '@/interfaces/token';
 import {getTokenRp} from '@/services/swap';
