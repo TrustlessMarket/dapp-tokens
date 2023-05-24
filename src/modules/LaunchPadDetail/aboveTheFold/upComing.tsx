0ca3ec0f942d606ea028d6e6d7bc9e56d8dd13aa
0xmegalodon <dung.thanh@autonomous.nyc>
2023-05-24 21:56:12 +0700

    add up comming ido

--- src/modules/LaunchPadDetail/aboveTheFold/upComing.tsx
+++ src/modules/LaunchPadDetail/aboveTheFold/upComing.tsx
@@ -1,22 +1,26 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import styles from './styles.module.scss';
-import {GridItem, SimpleGrid, Stat, StatLabel, StatNumber, Text} from "@chakra-ui/react";
+import {Box, GridItem, SimpleGrid, Stat, StatLabel, StatNumber, Text} from "@chakra-ui/react";
 import BodyContainer from "@/components/Swap/bodyContainer";
-import {useLaunchPadStatus} from "@/modules/Launchpad/Launchpad.Status";
 import {formatCurrency} from "@/utils";
 import moment from "moment";
 import Card from "@/components/Swap/card";
-import React, {useEffect, useState} from "react";
+import React, {useEffect, useRef, useState} from "react";
 import useCountDownTimer from "@/hooks/useCountdown";
 import {useDispatch} from "react-redux";
 import {requestReload} from "@/state/pnftExchange";
+import cx from 'classnames';
+import {CDN_URL} from "@/configs";
 
 const LaunchpadUpComing = ({poolDetail}: any) => {
-  const [status] = useLaunchPadStatus({ row: poolDetail });
   const [endTime, setEndTime] = useState(0);
   const [days, hours, minutes, seconds, expired] = useCountDownTimer(
     moment.unix(endTime).format("YYYY/MM/DD HH:mm:ss")
   );
+
+  const refVideo = useRef<any>();
+
+  const [isPlay, setIsPlay] = useState(false);
 
   useEffect(() => {
     if(poolDetail?.id) {
@@ -32,12 +36,43 @@ const LaunchpadUpComing = ({poolDetail}: any) => {
     }
   }, [expired]);
 
-  console.log('statusstatusstatus', status);
+  const onPlay = () => {
+    setIsPlay(true);
+    return refVideo.current.play();
+  };
+
+  const onPause = () => {
+    setIsPlay(false);
+    return refVideo.current.pause();
+  };
 
   return (
     <BodyContainer className={styles.wrapper}>
       <SimpleGrid className={"max-content"} columns={[1, 2]} spacingX={10}>
         <GridItem>
+          <img src={`${CDN_URL}/pages/gm/video-thumbnail.png`} alt="thumbnail" className={"home-image"}></img>
+          <Box
+            className="home-video"
+            flex={1}
+            position="relative"
+            display={["none", "block"]}
+            onClick={isPlay ? onPause : onPlay}
+            mt={4}
+          >
+            <video
+              poster={`${CDN_URL}/pages/gm/video-thumbnail.png`}
+              ref={refVideo}
+            >
+              <source
+                src={`${CDN_URL}/pages/gm/NBC_03.mp4`}
+                type="video/mp4"
+              ></source>
+            </video>
+            {!isPlay && <div className="bg-fade" />}
+            <a className={cx("btn-play", isPlay && "played")}>
+              <img src={`${CDN_URL}/icons/${isPlay ? 'ic_pause_video.svg' : 'ic_play_video.svg'}`} alt="thumbnail"></img>
+            </a>
+          </Box>
         </GridItem>
         <GridItem>
           <Card bgColor={"#1E1E22"} paddingX={8} paddingY={6}>
