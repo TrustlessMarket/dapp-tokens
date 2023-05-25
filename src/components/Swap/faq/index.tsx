687f6c474f6a0cdabd74541a0dc2c0ce31641ae5
0xmegalodon <dung.thanh@autonomous.nyc>
2023-05-25 14:00:28 +0700

    update UI

--- src/components/Swap/faq/index.tsx
+++ src/components/Swap/faq/index.tsx
@@ -23,12 +23,12 @@ const Item = ({ data } : {data: any}) => {
       <AccordionItem border='none' borderRadius={12} w={['100%', '760px']} bgColor='#1E1E22'>
         <AccordionButton px={[6,6]} pt={6} pb={open ? 4 : 6}>
           <Flex w='100%' alignItems='center'>
-            <IconPlusToMinus open={open} size='16px' color='text.accent' />
-            <Text ml={6} fontSize='lg' fontWeight='medium' textAlign={"left"} className={cx('faq-question')}>{data.q}</Text>
+            <IconPlusToMinus open={open} size='16px' color='#1b77fd' />
+            <Text ml={6} fontSize='lg' fontWeight='medium' textAlign={"left"} className={cx('faq-question')}>{data?.q}</Text>
           </Flex>
         </AccordionButton>
         <AccordionPanel pl={16} pr={6} pb={6} style={{paddingTop: '0'}}>
-          <Box className={cx(styles.answer, 'faq-answer')} color='text.secondary' fontSize='sm' dangerouslySetInnerHTML={{ __html: data.a }} />
+          <Box className={cx(styles.answer, 'faq-answer')} color='text.secondary' fontSize='sm' dangerouslySetInnerHTML={{ __html: data?.a }} />
         </AccordionPanel>
       </AccordionItem>
     </Accordion>
@@ -38,7 +38,7 @@ const Item = ({ data } : {data: any}) => {
 const Section = ({title, data}: {title?: any, data: any}) => {
   const [viewAll, setViewAll] = useState(false);
 
-  const renderData = viewAll ? data : [data[0], data[1], data[2], data[3], data[4]];
+  const renderData = viewAll || data?.length < 5 ? data : [data[0], data[1], data[2], data[3], data[4]];
 
   return (
       <Flex direction='column'>
@@ -46,7 +46,15 @@ const Section = ({title, data}: {title?: any, data: any}) => {
         <Flex gap={2} direction='column' mt={4}>
           {renderData.map((e: any , i: number ) => <Item key={i} data={e} />)}
         </Flex>
-        <Button color={"#FFFFFF"} mt={4} fontSize='md' fontWeight='medium' variant={"outline"} borderRadius={"8px"} h={"56px"} onClick={() => setViewAll(!viewAll)}>{viewAll ? 'View Less' : 'View All'}</Button>
+        <Button
+          color={"#FFFFFF"} mt={4} fontSize='md' fontWeight='medium'
+          variant={"outline"} borderRadius={"8px"} h={"56px"}
+          border={"1px solid #1b77fd"}
+          _hover={{
+            backgroundColor: '#1E1E22',
+          }}
+          onClick={() => setViewAll(!viewAll)}>{viewAll ? 'View Less' : 'View All'}
+        </Button>
       </Flex>
   )
 }
@@ -54,8 +62,8 @@ const Section = ({title, data}: {title?: any, data: any}) => {
 const FAQs = ({data, desc} : {data: any, desc?: any}) => {
   return (
     <Flex direction='column' alignItems='center'>
-      <Heading><Flex alignItems={"center"} gap={4}>FAQs</Flex></Heading>
-      <Text my={desc ? 8 : 5} maxW='800px' textAlign={['left', 'center']} color='#FFFFFF'>{desc}</Text>
+      {/*<Heading><Flex alignItems={"center"} gap={4}>FAQs</Flex></Heading>*/}
+      {/*<Text my={desc ? 8 : 5} maxW='800px' textAlign={['left', 'center']} color='#FFFFFF'>{desc}</Text>*/}
       <Section data={data}/>
     </Flex>
   )
