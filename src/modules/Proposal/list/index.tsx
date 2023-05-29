f5db482e26ce1a5bf4c6d9c1438cbc034648453e
0xmegalodon <dung.thanh@autonomous.nyc>
2023-05-29 11:40:34 +0700

    Merge remote-tracking branch 'origin/feature/ido' into develop
    # Conflicts:
    #	src/components/Social/index.tsx

---
+++ src/modules/Proposal/list/index.tsx
@@ -0,0 +1,12 @@
+import BodyContainer from "@/components/Swap/bodyContainer";
+import styles from './styles.module.scss';
+
+const ProposalList = () => {
+  return (
+    <BodyContainer className={styles.wrapper}>
+      ProposalList
+    </BodyContainer>
+  );
+};
+
+export default ProposalList;
---
+++ src/pages/proposal/index.tsx
@@ -0,0 +1,20 @@
+import {CDN_URL} from '@/configs';
+import Layout from '@/layouts';
+import Head from 'next/head';
+import React from 'react';
+import ProposalList from "@/modules/Proposal/list";
+
+const IdoToken = () => {
+  return (
+    <>
+      <Head>
+        <link rel="icon" href={`${CDN_URL}/icons/logo-tc-market.svg`} />
+      </Head>
+      <Layout>
+        <ProposalList />
+      </Layout>
+    </>
+  );
+};
+
+export default IdoToken;
