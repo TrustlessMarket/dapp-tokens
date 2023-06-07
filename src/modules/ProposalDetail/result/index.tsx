e30987e3f07c07e3b1d253f4be76ce626dae0813
0xmegalodon <dung.thanh@autonomous.nyc>
2023-06-07 22:25:52 +0700

    update UI

--- src/modules/ProposalDetail/result/index.tsx
+++ src/modules/ProposalDetail/result/index.tsx
@@ -4,12 +4,14 @@ import React, {useEffect, useState} from "react";
 import {useAppSelector} from "@/state/hooks";
 import {selectPnftExchange} from "@/state/pnftExchange";
 import Side from "@/modules/ProposalDetail/result/side";
-import {Box, Card, GridItem, SimpleGrid} from "@chakra-ui/react";
+import {Box} from "@chakra-ui/react";
 import styles from './styles.module.scss';
 import cx from 'classnames';
 import {getVoteResultLaunchpad} from "@/services/launchpad";
 import {ILaunchpad} from '@/interfaces/launchpad';
 import px2rem from "@/utils/px2rem";
+import {LAUNCHPAD_STATUS} from "@/modules/Launchpad/Launchpad.Status";
+import BuyForm from "@/modules/ProposalDetail/form";
 
 const ProposalResult = ({poolDetail}: { poolDetail: ILaunchpad }) => {
   const needReload = useAppSelector(selectPnftExchange).needReload;
@@ -36,71 +38,20 @@ const ProposalResult = ({poolDetail}: { poolDetail: ILaunchpad }) => {
   ]);
 
   return (
-    <Box>
-      <SimpleGrid columns={1} gap={6}>
-        <GridItem>
-          <Card bgColor={"transparent"} paddingX={10} paddingTop={10} paddingBottom={px2rem(100)} border={"1px solid #353945"} borderRadius={0}>
-            <Side
-              title={"For"}
-              totalVote={Number(poolDetail?.voteGoal)}
-              data={voteResult}
-              className={cx(styles.sideWrapper, styles.sideFor)}
-            />
-          </Card>
-        </GridItem>
-        {/*<GridItem>
-          <Card bgColor={"transparent"} paddingX={6} paddingY={6} border={"1px solid #353945"}>
-            <Side
-              title={"Against"}
-              totalVote={voteResult?.forVote?.totalVoter + voteResult?.againstVote?.totalVoter}
-              data={voteResult?.againstVote}
-              className={cx(styles.sideWrapper, styles.sideAgainst)}
-            />
-          </Card>
-        </GridItem>*/}
-        {/*<GridItem>
-          <Card bgColor={"transparent"} paddingX={6} paddingY={6} border={"1px solid #353945"} h={"100%"}>
-            <Box color={"#FFFFFF"}>
-              <Flex fontSize={px2rem(24)} fontWeight={500} alignItems={"center"} gap={1}>
-                <Text color={"rgba(255, 255, 255, 0.7)"} >Current results:</Text>
-                <Text color={result === 1 ? '#04C57F' : result === -1 ? '#BB0505' : '#FFFFFF'}>{result === 1 ? 'For' : result === -1 ? 'Against' : 'Equal'}</Text>
-              </Flex>
-              <Flex gap={3} mt={2}>
-                <Flex gap={3} className={cx(styles.btnResult, result === 1 && styles.btnResultFor)}>
-                  <Text>For</Text>
-                  <Text>{voteResult?.forVote?.totalVoter} vote{voteResult?.forVote?.totalVoter > 1 ? 's' : ''}</Text>
-                </Flex>
-                <Flex gap={3} className={cx(styles.btnResult, result === -1 && styles.btnResultAgainst)}>
-                  <Text>Against</Text>
-                  <Text>{voteResult?.againstVote?.totalVoter} vote{voteResult?.againstVote?.totalVoter > 1 ? 's' : ''}</Text>
-                </Flex>
-              </Flex>
-              <Box mt={8}>
-                <Flex gap={1} fontSize={px2rem(24)} fontWeight={500}>
-                  <Text color={"rgba(255, 255, 255, 0.7);"}>End:</Text>
-                  <Text>{moment(voteResult?.userProposal?.voteEnd).format('LL')}</Text>
-                </Flex>
-                {
-                  status.value === 'pending' ? (
-                    <Flex className={styles.boxTime} gap={1} alignItems={"center"}>
-                      <MdLocalFireDepartment fontSize={px2rem(20)}/>
-                      <CountDownTimer end_time={voteResult?.userProposal?.voteStart}/>
-                    </Flex>
-
-                  ) : status.value === 'active' ? (
-                    <Flex className={styles.boxTime} gap={1} alignItems={"center"}>
-                      <MdLocalFireDepartment fontSize={px2rem(20)}/>
-                      <CountDownTimer end_time={voteResult?.userProposal?.voteEnd}/>
-                    </Flex>
-                  ) : (
-                    <Box></Box>
-                  )
-                }
-              </Box>
-            </Box>
-          </Card>
-        </GridItem>*/}
-      </SimpleGrid>
+    <Box bgColor={"transparent"} paddingX={10} paddingTop={10} paddingBottom={px2rem(100)} border={"1px solid #353945"}>
+      {
+        [LAUNCHPAD_STATUS.Voting].includes(poolDetail?.state) && (
+          <Box mb={12}>
+            <BuyForm poolDetail={poolDetail}/>
+          </Box>
+        )
+      }
+      <Side
+        title={"For"}
+        totalVote={Number(poolDetail?.voteGoal)}
+        data={voteResult}
+        className={cx(styles.sideWrapper, styles.sideFor)}
+      />
     </Box>
   );
 }
