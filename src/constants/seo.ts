8efd5bb5bbc6f34865f7aa921ebba67eb4b18a11
0xmegalodon <stephen@newbitcoincity.com>
2023-06-09 12:33:08 +0700

    update brand name

--- src/constants/seo.ts
+++ src/constants/seo.ts
@@ -2,5 +2,5 @@ import { CDN_URL } from '@/configs';
 
 export const SEO_TITLE = 'New Bitcoin DEX';
 export const SEO_DESCRIPTION =
-  'New Bitcoin DEX is a decentralized exchange platform that operates on the Bitcoin blockchain through the use of trustless computers. With New Bitcoin DEX, you can easily swap cryptocurrencies, join a launchpad, gain voting rights in DAO and so much more!';
+  'New Bitcoin DEX is the first decentralized exchange platform that operates on the Bitcoin blockchain through the use of trustless computers. With New Bitcoin DEX, you can easily swap cryptocurrencies, join a launchpad, gain voting rights in DAO and so much more!';
 export const SEO_IMAGE = `${CDN_URL}/trustless-market/trustless-market-seo.png`;
