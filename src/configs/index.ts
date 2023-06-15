b541a32991a1f957e7b3d41b284c3ddf5ec18cbb
0xmegalodon <stephen@newbitcoincity.com>
2023-06-15 15:06:18 +0700

    scan

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -12,6 +12,7 @@ export const WALLET_URL: string = process.env.NEXT_PUBLIC_WALLET_URL!;
 export const TRUSTLESS_MARKET_URL: string = process.env.NEXT_PUBLIC_TRUSTLESS_MARKET_URL!;
 export const CHAIN_ID: string = process.env.NEXT_PUBLIC_CHAIN_ID!;
 export const ALCHEMY_API_KEY: string = process.env.NEXT_PUBLIC_ALCHEMY_API_KEY!;
+export const ETHERSCAN_URL: string = process.env.NEXT_PUBLIC_ETHERSCAN_URL!;
 
 // Contract configs
 export const ARTIFACT_CONTRACT: string = process.env.NEXT_PUBLIC_ARTIFACT_CONTRACT!;
@@ -32,5 +33,4 @@ export const DEFAULT_FROM_ADDRESS = process.env.NEXT_PUBLIC_DEFAULT_FROM_ADDRESS
 export const DEFAULT_TO_ADDRESS = process.env.NEXT_PUBLIC_DEFAULT_ADDRESS!;
 
 export const MEMPOOL_URL = 'https://mempool.space';
-export const ETHERSCAN_URL = 'https://etherscan.io';
 /* eslint-enable @typescript-eslint/no-non-null-assertion */
