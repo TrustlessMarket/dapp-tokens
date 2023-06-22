a27e31944b8619358a64db3561ecd10c587b7489
0xmegalodon <stephen@newbitcoincity.com>
2023-06-22 15:18:47 +0700

    update copy

--- src/modules/LaunchPadDetail/form/index.tsx
+++ src/modules/LaunchPadDetail/form/index.tsx
@@ -1280,7 +1280,7 @@ const BuyForm = ({ poolDetail }: { poolDetail: ILaunchpad }) => {
     if([LAUNCHPAD_STATUS.Launching].includes(poolDetail?.state)
       && moment(poolDetail.launchEnd).subtract("1", "h").isSameOrBefore(moment())
     ) {
-      toast.error('You can not contribute anymore!');
+      toast.error('Sorry! The time to contribute has ended.');
       return;
     }
 
