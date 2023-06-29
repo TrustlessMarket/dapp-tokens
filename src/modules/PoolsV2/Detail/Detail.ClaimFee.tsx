5feee6f73b2f2044ba353c060693a5bd07cc88ba
0xmegalodon <stephen@newbitcoincity.com>
2023-06-29 14:36:10 +0700

    update code collect fee

--- src/modules/PoolsV2/Detail/Detail.ClaimFee.tsx
+++ src/modules/PoolsV2/Detail/Detail.ClaimFee.tsx
@@ -10,8 +10,15 @@ import {useWindowSize} from "@trustless-computer/dapp-core";
 import {useAppDispatch, useAppSelector} from "@/state/hooks";
 import s from "@/modules/PoolsV2/Remove/styles.module.scss";
 import Card from "@/components/Swap/card";
-import {selectPnftExchange} from "@/state/pnftExchange";
+import {requestReload, selectPnftExchange, updateCurrentTransaction} from "@/state/pnftExchange";
 import useGetEarnedFee from "@/hooks/contract-operations/pools/v3/useGetEarnedFee";
+import useCollectFeeV3, {ICollectFeeV3} from "@/hooks/contract-operations/pools/v3/useCollectFee";
+import {transactionType} from "@/components/Swap/alertInfoProcessing/types";
+import {TransactionStatus} from "@/components/Swap/alertInfoProcessing/interface";
+import {logErrorToServer} from "@/services/swap";
+import {toastError} from "@/constants/error";
+import {showError} from "@/utils/toast";
+import {useWeb3React} from "@web3-react/core";
 
 const DetailClaimFee: React.FC<IDetailPositionBase> = ({ positionDetail }) => {
   const dispatch = useAppDispatch();
@@ -21,6 +28,9 @@ const DetailClaimFee: React.FC<IDetailPositionBase> = ({ positionDetail }) => {
   const token1Obj = positionDetail?.pair?.token1Obj;
   const needReload = useAppSelector(selectPnftExchange).needReload;
   const { call: getEarnedFee } = useGetEarnedFee();
+  const { call: collectFeeV3 } = useCollectFeeV3();
+  const { account } = useWeb3React();
+  const [submitting, setSubmitting] = useState(false);
 
   useEffect(() => {
     if (positionDetail?.tokenId) {
@@ -32,6 +42,48 @@ const DetailClaimFee: React.FC<IDetailPositionBase> = ({ positionDetail }) => {
     const res = await getEarnedFee({ tokenId: positionDetail?.tokenId });
     setEarnedFees(res);
   };
+
+  const handleCollectFee = async () => {
+    try {
+      setSubmitting(true);
+      dispatch(
+        updateCurrentTransaction({
+          id: transactionType.collectFee,
+          status: TransactionStatus.info,
+        }),
+      );
+
+      const params: ICollectFeeV3 = {
+        tokenId: positionDetail?.tokenId,
+      };
+
+      const response: any = await collectFeeV3(params);
+      dispatch(
+        updateCurrentTransaction({
+          id: transactionType.collectFee,
+          status: TransactionStatus.success,
+          hash: response.hash,
+          infoTexts: {
+            success: `Collect fee successfully.`
+          },
+        }),
+      );
+      dispatch(requestReload());
+    } catch (err) {
+      dispatch(updateCurrentTransaction(null));
+      const message =
+        (err as Error).message || 'Something went wrong. Please try again later.';
+      logErrorToServer({
+        type: 'error',
+        address: account,
+        error: JSON.stringify(err),
+        message: message,
+      });
+      toastError(showError, err, { address: account });
+    } finally {
+      setSubmitting(false);
+    }
+  }
 
   const confirmCollectFees = () => {
     const id = 'modalCollectFees';
@@ -104,16 +156,17 @@ const DetailClaimFee: React.FC<IDetailPositionBase> = ({ positionDetail }) => {
                   />
                 </Flex>
               </Card>
-              <Text fontSize={'sm'} color={'rgb(152, 161, 192)'} fontStyle={"italic"} mt={2}>
+              <Text fontSize={'sm'} color={'rgb(152, 161, 192)'} fontStyle={"italic"} mt={2} mb={"8px !important"}>
                 Collecting fees will withdraw currently available fees for you.
               </Text>
               <FiledButton
                 loadingText="Processing"
                 btnSize={'h'}
-                mt={4}
-                onClick={() => {
-
+                containerConfig={{ flex: 1, mt: 4 }}
+                processInfo={{
+                  id: transactionType.collectFee,
                 }}
+                onClick={handleCollectFee}
               >
                 Collect
               </FiledButton>
