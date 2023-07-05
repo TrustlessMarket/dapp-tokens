a76039fba7de6e2e03142eed8553dbd271ce332c
Zestpond <tanmynguyen2015@gmail.com>
2023-07-05 10:54:58 +0700

    fix: update

--- src/modules/Launchpad/index.tsx
+++ src/modules/Launchpad/index.tsx
@@ -65,7 +65,7 @@ const LaunchpadContainer = () => {
 
   useEffect(() => {
     getData();
-  }, [needReload, account]);
+  }, [needReload, account, currentChain?.chain]);
 
   const getData = async () => {
     try {
