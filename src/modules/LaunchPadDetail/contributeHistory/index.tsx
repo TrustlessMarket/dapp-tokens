b059bca495a48d789ff0173d8cec782a529c6c39
tuan <tuan@autonomous.nyc>
2023-07-10 17:24:51 +0700

    update launchpad btc

--- src/modules/LaunchPadDetail/contributeHistory/index.tsx
+++ src/modules/LaunchPadDetail/contributeHistory/index.tsx
@@ -1,12 +1,16 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import styles from "./styles.module.scss";
 import DepositHistory from "./Deposit.History";
+import DepositBtcHistory from "./Deposit.Btc.History";
 import ContributeHistory from "./Contribute.History";
 import {Box, Tab, TabList, TabPanel, TabPanels, Tabs} from "@chakra-ui/react";
 import {IResourceChain} from "@/interfaces/chain";
 import {useSelector} from "react-redux";
 import {selectPnftExchange} from "@/state/pnftExchange";
-
+import {
+  compareString,
+  getWETHAddress
+} from '@/utils';
 const TMTransferHistory = (props: any) => {
   const { poolDetail } = props;
   const currentChain: IResourceChain = useSelector(selectPnftExchange).currentChain;
@@ -21,14 +25,15 @@ const TMTransferHistory = (props: any) => {
         <Tabs isFitted variant='soft-rounded'>
           <TabList mb={6} mt={6}>
             <Tab>{currentChain?.chain} Transaction</Tab>
-            <Tab>ETH Transaction</Tab>
+            <Tab>{compareString(poolDetail?.liquidityTokenAddress,getWETHAddress())?"ETH":"BITCOIN"} Transaction</Tab>
           </TabList>
           <TabPanels>
             <TabPanel>
               <ContributeHistory poolDetail={poolDetail} />
             </TabPanel>
             <TabPanel>
-              <DepositHistory poolDetail={poolDetail} />
+              {compareString(poolDetail?.liquidityTokenAddress,getWETHAddress())?
+                  <DepositHistory poolDetail={poolDetail} />: <DepositBtcHistory poolDetail={poolDetail} />}
             </TabPanel>
           </TabPanels>
         </Tabs>
