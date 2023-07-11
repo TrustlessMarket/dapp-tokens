0acf6c55a9d6d1c297437929bc7a911c6931cc38
tuan <tuan@autonomous.nyc>
2023-07-11 10:29:48 +0700

    fix: update launchpad btc

---
+++ src/modules/LaunchPadDetail/contributeHistory/Deposit.Btc.History.tsx
@@ -0,0 +1,164 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import ListTable, {ColumnProp} from '@/components/Swap/listTable';
+import { formatLongAddress} from '@/utils';
+import {Flex, Text} from '@chakra-ui/react';
+import {useEffect, useMemo, useState} from 'react';
+import {useWeb3React} from "@web3-react/core";
+
+import {getLaunchpadDepositAddress} from "@/services/launchpad";
+import {useSelector} from "react-redux";
+import {getIsAuthenticatedSelector} from "@/state/user/selector";
+import {colors} from "@/theme/colors";
+import {isProduction} from "@/utils/commons";
+
+
+
+const DepositBtcHistory = (props: any) => {
+  const { poolDetail } = props;
+  const [list, setList] = useState<any[]>([]);
+  const { account } = useWeb3React();
+  const [depositAddressInfo, setDepositAddressInfo] = useState<any>();
+  const isAuthenticated = useSelector(getIsAuthenticatedSelector);
+  const [loading, setLoading] = useState(true);
+  const testnet = isProduction() ?"":"/testnet"
+
+      useEffect(() => {
+    if(account && poolDetail?.id) {
+      fetchData();
+    }
+  }, [account, isAuthenticated, poolDetail?.id]);
+
+  useEffect(() => {
+    if(depositAddressInfo?.depositAddress) {
+      getList();
+    }
+  }, [depositAddressInfo?.depositAddress]);
+
+  const fetchData = async () => {
+    try {
+      const response: any = await Promise.all([
+        getLaunchpadDepositAddress({
+          network: 'bitcoin',
+          address: account,
+          launchpad_id: poolDetail?.id,
+        }),
+      ]);
+      setDepositAddressInfo(response[0]);
+    } catch (error) {
+      console.log('Launchpad detail form fetchData', error);
+    }
+  };
+
+  const getList = async () => {
+    try {
+      const res = await fetch(
+          `https://blockstream.info${testnet}/api/address/${depositAddressInfo?.depositAddress}/txs`,
+      ).then((res) => {
+        return res.json();
+      });
+      for(let  index = res.length-1; index>=0; index--)
+      {
+        let isOutput = false;
+        while (res[index].vout.length>0){
+          if (res[index].vout[0].scriptpubkey_address == depositAddressInfo?.depositAddress) {
+            isOutput = true
+            break;
+          }
+          else{
+            res[index].vout.splice(0, 1);
+          }
+        }
+        if(!isOutput) {
+          res.splice(index, 1);
+        }
+      }
+
+      setList(res || []);
+    } catch (error) {
+
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const columns: ColumnProp[] = useMemo(
+    () => [
+      {
+        id: 'txHash',
+        label: 'Transaction',
+        labelConfig: {
+          fontSize: '12px',
+          fontWeight: '500',
+          color: '#B1B5C3',
+        },
+        config: {
+          borderBottom: 'none',
+        },
+        render(row: any) {
+          return (
+            <Flex direction={"column"} color={"#FFFFFF"}>
+              <a
+                title="explorer"
+                href={`https://blockstream.info${testnet}/tx/${row?.txid}`}
+                target="_blank"
+                style={{textDecoration: 'underline', color: colors.bluePrimary}}
+              >
+                {formatLongAddress(row?.txid)}
+              </a>
+            </Flex>
+          );
+        },
+      },
+      {
+        id: 'amount_in',
+        label: 'Amount',
+        labelConfig: {
+          fontSize: '12px',
+          fontWeight: '500',
+          color: '#B1B5C3',
+        },
+        config: {
+          borderBottom: 'none',
+        },
+        render(row: any) {
+          return (
+            <Text color={"#000000"}>
+              {row.vout[0].value/Math.pow(10,8)} BTC
+            </Text>
+          );
+        },
+      },
+      {
+        id: 'status',
+        label: 'Status',
+        labelConfig: {
+          fontSize: '12px',
+          fontWeight: '500',
+          color: '#B1B5C3',
+        },
+        config: {
+          borderBottom: 'none',
+        },
+        render(row: any) {
+          return <Text color={"rgb(0, 170, 108)"}>
+            {row?.status.confirmed?"Confirmed":"Unconfirmed"}
+          </Text>;
+        },
+      },
+    ],
+    [],
+  );
+
+  return (
+    <ListTable
+      data={list}
+      columns={columns}
+      initialLoading={loading}
+      showEmpty={true}
+      hideIcon={true}
+      theme={'light'}
+    />
+  );
+};
+
+export default DepositBtcHistory;
--- src/modules/LaunchPadDetail/depositEth/index.tsx
+++ src/modules/LaunchPadDetail/depositEth/index.tsx
@@ -12,67 +12,67 @@ import {useAppSelector} from "@/state/hooks";
 import {selectPnftExchange} from "@/state/pnftExchange";
 
 const DepositEth = (props: any) => {
-  const {address, onClose} = props;
-  const configs = useAppSelector(selectPnftExchange).configs;
+    const {address, onClose,network} = props;
+    const configs = useAppSelector(selectPnftExchange).configs;
 
-  return (
-    <Box className={styles.wrapper}>
-      <Text
-        fontSize={px2rem(16)}
-        fontWeight={400}
-        color={"#1C1C1C"}
-        opacity={0.7}
-      >
-        Here is your unique Ethereum address. You can send your ETH to this address from your Metamask or your Exchanges.
-      </Text>
-      <Flex justifyContent={"center"} mt={10} mb={10}>
-        <QRCodeSVG width={175} height={175} value={address} />
-      </Flex>
-      <Text
-        fontSize={px2rem(16)}
-        fontWeight={500}
-      >Address (ERC20)</Text>
-      <InputGroup size="sm" mt={1}>
-        <Input
-          value={address}
-          fontSize={px2rem(16)}
-          fontWeight={500}
-          disabled
-        />
-        <InputRightElement
-          mr={4}
-          height="100%"
-        >
-          <CopyToClipboard
-            onCopy={() => toast.success('Copied address!')}
-            text={address}
-          >
-            <Center
-              minW={px2rem(40)}
-              minH={px2rem(40)}
-              bgColor={"#F7F7FA"}
-              borderRadius={"8px"}
-              cursor={"pointer"}
+    return (
+        <Box className={styles.wrapper}>
+            <Text
+                fontSize={px2rem(16)}
+                fontWeight={400}
+                color={"#1C1C1C"}
+                opacity={0.7}
             >
-              <FiCopy />
-            </Center>
-          </CopyToClipboard>
-        </InputRightElement>
-      </InputGroup>
-      <Text
-        fontSize={px2rem(16)}
-        fontWeight={500}
-        mt={2}
-      >Fee: {configs?.lpEthFee} ETH</Text>
-      <FiledButton
-        btnSize={'h'}
-        onClick={onClose}
-        mt={6}
-      >
-        DONE
-      </FiledButton>
-    </Box>
-  );
+                Here is your unique {network} address. You can send your {network} to this address from your Metamask or your Exchanges.
+            </Text>
+            <Flex justifyContent={"center"} mt={10} mb={10}>
+                <QRCodeSVG width={175} height={175} value={address} />
+            </Flex>
+            <Text
+                fontSize={px2rem(16)}
+                fontWeight={500}
+            >Address {network}</Text>
+            <InputGroup size="sm" mt={1}>
+                <Input
+                    value={address}
+                    fontSize={px2rem(16)}
+                    fontWeight={500}
+                    disabled
+                />
+                <InputRightElement
+                    mr={4}
+                    height="100%"
+                >
+                    <CopyToClipboard
+                        onCopy={() => toast.success('Copied address!')}
+                        text={address}
+                    >
+                        <Center
+                            minW={px2rem(40)}
+                            minH={px2rem(40)}
+                            bgColor={"#F7F7FA"}
+                            borderRadius={"8px"}
+                            cursor={"pointer"}
+                        >
+                            <FiCopy />
+                        </Center>
+                    </CopyToClipboard>
+                </InputRightElement>
+            </InputGroup>
+            <Text
+                fontSize={px2rem(16)}
+                fontWeight={500}
+                mt={2}
+            >Fee: {network=="ethereum"?configs?.lpEthFee.toString()+" ETH":configs?.lpBtcFee.toString()+" BTC"}</Text>
+            <FiledButton
+                btnSize={'h'}
+                onClick={onClose}
+                mt={6}
+            >
+                DONE
+            </FiledButton>
+        </Box>
+    );
 };
 
 export default DepositEth;
