e08894cc7c0317cee4a4f91dd2a0beac7041ec4e
0xmegalodon <stephen@newbitcoincity.com>
2023-07-27 16:03:49 +0700

    launchpad get started nos

--- src/modules/LaunchPadGetStarted/index.tsx
+++ src/modules/LaunchPadGetStarted/index.tsx
@@ -1,22 +1,29 @@
-import styles from "./styles.module.scss";
-import BodyContainer from "@/components/Swap/bodyContainer";
-import SwapTokens from "./swapTokens";
-import SectionContainer from "@/components/Swap/sectionContainer";
-import {Box} from "@chakra-ui/react";
+import {useEffect} from "react";
+import {ROUTE_PATH} from "@/constants/route-path";
+import {useRouter} from "next/router";
+import {IResourceChain} from "@/interfaces/chain";
+import {useAppSelector} from "@/state/hooks";
+import {selectPnftExchange} from "@/state/pnftExchange";
+import {compareString} from "@/utils";
+import {L2_CHAIN_INFO} from "@/constants/chains";
+import GetStartedNos from './nos';
+import GetStartedTC from './tc';
 
-const LaunchPadGetStarted = () => {
-  return (
-    <BodyContainer className={styles.wrapper}>
-      <SectionContainer>
-        <div>
-          <h3 className={styles.upload_title}>Instructions for Participating in TM LaunchPad</h3>
-        </div>
-        <Box mt={6}>
-          <SwapTokens />
-        </Box>
-      </SectionContainer>
-    </BodyContainer>
+const GetStarted = () => {
+  const router = useRouter();
+  const currentChain: IResourceChain = useAppSelector(selectPnftExchange).currentChain;
+
+  useEffect(() => {
+    if(currentChain?.chain) {
+      router.replace(`${ROUTE_PATH.LAUNCHPAD_GET_STARTED}?network=${currentChain?.chain?.toLowerCase()}`)
+    }
+  }, [currentChain?.chain]);
+
+  return compareString(router?.query?.network, L2_CHAIN_INFO.chain) ?  (
+    <GetStartedNos />
+  ) : (
+    <GetStartedTC />
   );
 };
 
-export default LaunchPadGetStarted;
+export default GetStarted;
