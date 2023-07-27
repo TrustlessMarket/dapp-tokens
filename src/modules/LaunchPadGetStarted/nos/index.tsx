05c19db7672178025f86fa132a714262e1c98065
0xmegalodon <stephen@newbitcoincity.com>
2023-07-27 17:34:37 +0700

    launchpad get started nos

--- src/modules/LaunchPadGetStarted/nos/index.tsx
+++ src/modules/LaunchPadGetStarted/nos/index.tsx
@@ -2,7 +2,8 @@ import styles from "../styles.module.scss";
 import BodyContainer from "@/components/Swap/bodyContainer";
 import SwapTokens from "./swapTokens";
 import SectionContainer from "@/components/Swap/sectionContainer";
-import {Box} from "@chakra-ui/react";
+import {Box, Tab, TabList, TabPanel, TabPanels, Tabs} from "@chakra-ui/react";
+import CreateTokens from "./createTokens";
 
 const LaunchPadGetStarted = () => {
   return (
@@ -11,12 +12,33 @@ const LaunchPadGetStarted = () => {
         <div>
           <h3 className={styles.upload_title}>Instructions for Participating in New Bitcoin LaunchPad</h3>
         </div>
-        <Box mt={6}>
-          <SwapTokens />
+        <Box
+          className={styles.tabContainer}
+          // style={{
+          //   borderTop: `1px solid ${colors.darkBorderColor}`,
+          //   maxHeight: '300px',
+          // }}
+          flex={1}
+          mt={6}
+        >
+          <Tabs isFitted variant='soft-rounded'>
+            <TabList mb={6} mt={6}>
+              <Tab>Vote</Tab>
+              <Tab>Contribute</Tab>
+            </TabList>
+            <TabPanels>
+              <TabPanel>
+                <SwapTokens />
+              </TabPanel>
+              <TabPanel>
+                <CreateTokens />
+              </TabPanel>
+            </TabPanels>
+          </Tabs>
         </Box>
       </SectionContainer>
     </BodyContainer>
-  );
+  )
 };
 
 export default LaunchPadGetStarted;
